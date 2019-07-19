73188ec90e50bacf92a14d40430127bc9108dc22
yu lin <yulin_2004@hotmail.com>
2019-07-19 14:18:18 +0800

    Connect OIS to Query Panel Quantity

---
+++ ConnectOIS/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace ConnectOIS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnDB = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labelStart = new System.Windows.Forms.Label();
+            this.labelEnd = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.buttonQuery = new System.Windows.Forms.Button();
+            this.textBoxSQL = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonConnDB
+            // 
+            this.buttonConnDB.Location = new System.Drawing.Point(23, 37);
+            this.buttonConnDB.Name = "buttonConnDB";
+            this.buttonConnDB.Size = new System.Drawing.Size(124, 35);
+            this.buttonConnDB.TabIndex = 0;
+            this.buttonConnDB.Text = "Connect to OIS";
+            this.buttonConnDB.UseVisualStyleBackColor = true;
+            this.buttonConnDB.Click += new System.EventHandler(this.buttonConnDB_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 76);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(778, 227);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // labelStart
+            // 
+            this.labelStart.AutoSize = true;
+            this.labelStart.Location = new System.Drawing.Point(176, 21);
+            this.labelStart.Name = "labelStart";
+            this.labelStart.Size = new System.Drawing.Size(29, 13);
+            this.labelStart.TabIndex = 2;
+            this.labelStart.Text = "Start";
+            // 
+            // labelEnd
+            // 
+            this.labelEnd.AutoSize = true;
+            this.labelEnd.Location = new System.Drawing.Point(369, 24);
+            this.labelEnd.Name = "labelEnd";
+            this.labelEnd.Size = new System.Drawing.Size(26, 13);
+            this.labelEnd.TabIndex = 3;
+            this.labelEnd.Text = "End";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(179, 37);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(181, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(372, 37);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(168, 20);
+            this.dateTimePicker2.TabIndex = 5;
+            // 
+            // buttonQuery
+            // 
+            this.buttonQuery.Location = new System.Drawing.Point(609, 21);
+            this.buttonQuery.Name = "buttonQuery";
+            this.buttonQuery.Size = new System.Drawing.Size(142, 35);
+            this.buttonQuery.TabIndex = 6;
+            this.buttonQuery.Text = "Query Panel Quantity";
+            this.buttonQuery.UseVisualStyleBackColor = true;
+            this.buttonQuery.Click += new System.EventHandler(this.buttonQuery_Click);
+            // 
+            // textBoxSQL
+            // 
+            this.textBoxSQL.Location = new System.Drawing.Point(23, 1);
+            this.textBoxSQL.Name = "textBoxSQL";
+            this.textBoxSQL.Size = new System.Drawing.Size(147, 20);
+            this.textBoxSQL.TabIndex = 7;
+            this.textBoxSQL.Text = "HostName\\\\InstanceName";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 307);
+            this.Controls.Add(this.textBoxSQL);
+            this.Controls.Add(this.buttonQuery);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.labelEnd);
+            this.Controls.Add(this.labelStart);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonConnDB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnDB;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labelStart;
+        private System.Windows.Forms.Label labelEnd;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button buttonQuery;
+        private System.Windows.Forms.TextBox textBoxSQL;
+    }
+}
+
---
+++ ConnectOIS/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ConnectOIS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConnDB_Click(object sender, EventArgs e)
+        {
+            string strSQL = textBoxSQL.Text.ToString();
+            SqlConnection con = new SqlConnection("database=SIPLACEOIS;Password=Siplace%Sa.1.Pwd;User ID=sa;server="+strSQL+"");
+            SqlCommand cm = new SqlCommand();
+            cm.Connection = con;
+            cm.CommandText = "select * from BOARD";
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cm;
+            DataSet ds = new DataSet();
+            da.Fill(ds, "lPanel");
+            dataGridView1.DataSource = ds.Tables["lPanel"];
+        }
+
+        private void buttonQuery_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("database=SIPLACEOIS;Password=Siplace%Sa.1.Pwd;User ID=sa;server=DESKTOP-99L3CJR\\SIPLACE_2012EX");
+            SqlCommand cm = new SqlCommand();
+            dateTimePicker1.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            dateTimePicker2.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            cm.Connection = con;
+            cm.CommandText = "SELECT strRecipe as Panel,sum (lPanel)as Product_Panel_Quantity FROM[SiplaceOIS].[dbo].[BOARD] Where dtTime between '" + dateTimePicker1.Text + "' and '" + dateTimePicker2.Text + "' group by strRecipe";
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cm;
+            DataSet ds = new DataSet();
+            da.Fill(ds, "strRecipe as Panel,sum (lPanel)as Product_Panel_Quantity");
+            dataGridView1.DataSource = ds.Tables["strRecipe as Panel,sum (lPanel)as Product_Panel_Quantity"];
+        }
+    }
+}
